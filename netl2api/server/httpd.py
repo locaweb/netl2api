704b8e68f5d8dfcfc25b9f684da098d00b1062cd
Eduardo S. Scarpellini <eduardo.scarpellini@locaweb.com.br>
2013-01-10 18:24:59 -0200

    some improvements

--- netl2api/server/httpd.py
+++ netl2api/server/httpd.py
@@ -27,14 +27,13 @@ import os
 import sys
 import pwd
 from multiprocessing import Process
-from bottle import debug, run, route, get, put, delete, error, request, response, abort
+from bottle import ServerAdapter, debug, run, route, get, put, delete, error, request, response, abort
 from netl2api.server.http_cache import cached, invalidate_cache
 from netl2api.server.http_utils import reply_json, validate_input, context
 from netl2api.server.workers import switch_cfg_persistence
 from netl2api.server.workers.switch_cfg_persistence_utils import defer_save_switch_cfg
 from netl2api.lib.utils import get_switch_instance
 from netl2api.lib.config import get_netl2server_cfg, setup_netl2server_logger, get_devices_cfg
-
 
 cfg          = get_netl2server_cfg()
 logger       = setup_netl2server_logger(cfg)
@@ -44,24 +43,43 @@ netl2debug   = cfg.get("logger", "level").lower() == "debug"
 RE_TYPE_VLAN_TAGGED = re.compile(r"^(?:True|False)$", re.IGNORECASE)
 
 
+def log_request_ahead(msg=None, msg_args=None):
+    """ use @log_request_ahead between @authorize and @cached """
+    def proxy(f):
+        def log(*args, **kwargs):
+            if msg is not None:
+                lmsg      = msg
+                lmsg_args = msg_args
+                if msg_args is not None:
+                    lmsg = lmsg % tuple([kwargs.get(a) for a in lmsg_args])
+                logger.info("%s -- context: %s" % (lmsg, request["context"]))
+            return f(*args, **kwargs)
+        return log
+    return proxy
+
+
 # Force Exception if using devices.cfg and permissions are wrong
-dev_cfg = get_devices_cfg()
+if cfg.get("devices", "use_redis").lower() == "false":
+    dev_cfg = get_devices_cfg()
+
 @get("/devices")
 @context
+@log_request_ahead("Listing available devices")
 @reply_json
 def devices_list():
     # MUST return ONLY switch names -- for CLI completion purpose
-    logger.info("Listing available devices -- context: %s" % request["context"])
+    #logger.info("Listing available devices -- context: %s" % request["context"])
     return get_devices_cfg().keys()
 
 
 @get("/info/<device>")
 @context
+@log_request_ahead("Showing generic information for device %s", ("device",))
 @reply_json
-@cached(ttl=3600)
+@cached(ttl=86400)
 def device_info(device=None):
-    logger.info("Showing generic information for device %s -- context: %s" %\
-                    (device, request["context"]))
+    #logger.info("Showing generic information for device %s -- context: %s" %\
+    #                (device, request["context"]))
     swinfo = {}
     swinst = get_switch_instance(device)
     swinfo["hostname"] = swinst.show_hostname()
@@ -76,11 +94,12 @@ def device_info(device=None):
 
 @get("/version/<device>")
 @context
+@log_request_ahead("Showing version information from device %s", ("device",))
 @reply_json
-@cached(ttl=3600)
+@cached(ttl=86400)
 def show_version(device=None):
-    logger.info("Showing version information from device '%s' -- context: %s" %\
-                     (device, request["context"]))
+    #logger.info("Showing version information from device '%s' -- context: %s" %\
+    #                 (device, request["context"]))
     swinst = get_switch_instance(device)
     defer_save_switch_cfg(device)
     return swinst.show_version()
@@ -88,11 +107,12 @@ def show_version(device=None):
 
 @get("/system/<device>")
 @context
+@log_request_ahead("Showing system information from device '%s'", ("device",))
 @reply_json
-@cached(ttl=3600)
+@cached(ttl=86400)
 def show_system(device=None):
-    logger.info("Showing system information from device '%s' -- context: %s" %\
-                     (device, request["context"]))
+    #logger.info("Showing system information from device '%s' -- context: %s" %\
+    #                 (device, request["context"]))
     swinst = get_switch_instance(device)
     return swinst.show_system()
 
@@ -108,25 +128,27 @@ def interfaces_route_actions(device=None, remaining_path=None):
         if m is None:
             abort(404, "Not Found")
         route_act = m.group(2)
+        interface_id=m.group(1).lower()
         if route_act == "attach_vlan":
-            return interface_attach_vlan(device=device, interface_id=m.group(1))
+            return interface_attach_vlan(device=device, interface_id=interface_id)
         if route_act == "detach_vlan":
-            return interface_detach_vlan(device=device, interface_id=m.group(1))
+            return interface_detach_vlan(device=device, interface_id=interface_id)
         if route_act == "change_description":
-            return change_interface_description(device=device, interface_id=m.group(1))
+            return change_interface_description(device=device, interface_id=interface_id)
         if route_act == "enable":
-            return enable_interface(device=device, interface_id=m.group(1))
+            return enable_interface(device=device, interface_id=interface_id)
         if route_act == "disable":
-            return disable_interface(device=device, interface_id=m.group(1))
+            return disable_interface(device=device, interface_id=interface_id)
         abort(404, "Not Found")
     abort(405, "Method Not Allowed")
 
 
+@log_request_ahead("Showing interfaces informations from device '%s'", ("device",))
 @reply_json
 @cached(ttl=3600)
 def show_interfaces(device=None, interface_id=None):
-    logger.info("Showing interfaces informations from device '%s' -- context: %s" %\
-                     (device, request["context"]))
+    #logger.info("Showing interfaces informations from device '%s' -- context: %s" %\
+    #                 (device, request["context"]))
     swinst = get_switch_instance(device)
     return swinst.show_interfaces(interface_id=interface_id)
 
@@ -232,11 +254,12 @@ def destroy_vlan(device=None, vlan_id=None):
 
 @get(["/vlans/<device>", "/vlans/<device>/<vlan_id>"])
 @context
+@log_request_ahead("Showing VLAN information from device %s", ("device",))
 @reply_json
 @cached(ttl=3600)
 def show_vlans(device=None, vlan_id=None):
-    logger.info("Showing VLAN information from device '%s' -- context: %s" %\
-                    (device, request["context"]))
+    #logger.info("Showing VLAN information from device '%s' -- context: %s" %\
+    #                (device, request["context"]))
     swinst = get_switch_instance(device)
     return swinst.show_vlans(vlan_id=vlan_id)
 
@@ -309,11 +332,12 @@ def destroy_lag(device=None, lag_id=None):
 
 @get(["/lags/<device>", "/lags/<device>/<lag_id>"])
 @context
+@log_request_ahead("Showing LAG information from device %s", ("device",))
 @reply_json
 @cached(ttl=3600)
 def show_lags(device=None, lag_id=None):
-    logger.info("Showing LAG information from device '%s' -- context: %s" %\
-                     (device, request["context"]))
+    #logger.info("Showing LAG information from device '%s' -- context: %s" %\
+    #                 (device, request["context"]))
     swinst = get_switch_instance(device)
     return swinst.show_lags(lag_id=lag_id)
 
@@ -343,8 +367,8 @@ def disable_lag(device=None, lag_id=None):
 
 
 @put("/lags/<device>/<lag_id>/attach_interface")
-@validate_input(src="forms", interface_id=str)
 @context
+@validate_input(src="forms", interface_id=str)
 @reply_json
 def lag_attach_interface(device=None, lag_id=None):
     logger.info("Attaching a new interface to LAG '%s' in device '%s' -- context: %s" %\
@@ -357,8 +381,8 @@ def lag_attach_interface(device=None, lag_id=None):
 
 
 @put("/lags/<device>/<lag_id>/detach_interface")
-@validate_input(src="forms", interface_id=str)
 @context
+@validate_input(src="forms", interface_id=str)
 @reply_json
 def lag_detach_interface(device=None, lag_id=None):
     logger.info("Detaching an interface from LAG '%s' in device '%s' -- context: %s" %\
@@ -371,8 +395,8 @@ def lag_detach_interface(device=None, lag_id=None):
 
 
 @put("/lags/<device>/<lag_id>/attach_vlan")
-@validate_input(src="forms", vlan_id=int, tagged=RE_TYPE_VLAN_TAGGED)
 @context
+@validate_input(src="forms", vlan_id=int, tagged=RE_TYPE_VLAN_TAGGED)
 @reply_json
 def lag_attach_vlan(device=None, lag_id=None):
     logger.info("Attaching a new VLAN to LAG '%s' in device '%s' -- context: %s" %\
@@ -382,12 +406,12 @@ def lag_attach_vlan(device=None, lag_id=None):
     swinst  = get_switch_instance(device)
     swinst.lag_attach_vlan(lag_id=lag_id, vlan_id=vlan_id, tagged=tagged)
     defer_save_switch_cfg(device)
-    invalidate_cache("/lags/%s" % device)
+    invalidate_cache("/vlans/%s" % device)
 
 
 @put("/lags/<device>/<lag_id>/detach_vlan")
-@validate_input(src="forms", vlan_id=int, tagged=RE_TYPE_VLAN_TAGGED)
 @context
+@validate_input(src="forms", vlan_id=int, tagged=RE_TYPE_VLAN_TAGGED)
 @reply_json
 def lag_detach_vlan(device=None, lag_id=None):
     logger.info("Detaching a VLAN from LAG '%s' in device '%s' -- context: %s" %\
@@ -397,20 +421,21 @@ def lag_detach_vlan(device=None, lag_id=None):
     swinst  = get_switch_instance(device)
     swinst.lag_detach_vlan(lag_id=lag_id, vlan_id=vlan_id, tagged=tagged)
     defer_save_switch_cfg(device)
-    invalidate_cache("/lags/%s" % device)
+    invalidate_cache("/vlans/%s" % device)
 
 
-# @get(["/networkpath/<from_device>", "/networkpath/<from_device>/<to_device>"])
-# @context
-# @reply_json
-# @cached(ttl=120)
-# def trace_network_path(from_device=None, to_device=None):
-#     logger.info("Tracing network-path from device '%s' to '%s'  -- context: %s" %\
-#                      (from_device, to_device, request["context"]))
-#     network_paths = find_network_paths(graph_repr(from_device=from_device),
-#                                                   from_device=from_device, to_device=to_device)
-#     #logger.debug("Path from device '%s' to device '%s': %s" % (from_device, to_device, network_paths))
-#     return network_paths
+#@get(["/networkpath/<from_device>", "/networkpath/<from_device>/<to_device>"])
+#@context
+#@log_request_ahead("Tracing network-path from device '%s' to '%s'", ("from_device", "to_device"))
+#@reply_json
+#@cached(ttl=86400)
+#def trace_network_path(from_device=None, to_device=None):
+#    #logger.info("Tracing network-path from device '%s' to '%s'  -- context: %s" %\
+#    #                 (from_device, to_device, request["context"]))
+#    network_paths = find_network_paths(graph_repr(from_device=from_device),
+#                                                  from_device=from_device, to_device=to_device)
+#    #logger.debug("Path from device '%s' to device '%s': %s" % (from_device, to_device, network_paths))
+#    return network_paths
 
 
 @error(400)
@@ -449,13 +474,41 @@ def error500(err):
     return err_info
 
 
+class PasteServerAdapter(ServerAdapter):
+    def run(self, handler): # pragma: no cover
+        from paste import httpserver
+        if not self.quiet:
+            from paste.translogger import TransLogger
+            handler = TransLogger(handler)
+        httpserver.serve(handler, host=self.host, port=str(self.port), protocol_version="HTTP/1.1",
+                        daemon_threads=True, socket_timeout=600,
+                        use_threadpool=cfg.get("httpd", "use_threadpool").lower() == "true",
+                        threadpool_workers=cfg.getint("httpd", "threadpool_workers"),
+                        threadpool_options={ "spawn_if_under":    cfg.getint("httpd", "threadpool_workers")/2,
+                                             "hung_check_period": 60,
+                                             "kill_thread_limit": 900 },
+                         **self.options)
+
+
 def start_workers():
     if cfg.get("job.switch_cfg_persistence", "enabled") == "true":
-        p_switch_cfg_persistence = Process(target=switch_cfg_persistence.daemon)
+        p_switch_cfg_persistence = Process(target=switch_cfg_persistence.daemon,
+                                           name="netl2api [netl2server:http-daemon/job/switch-cfg-persistence]")
         p_switch_cfg_persistence.start()
-        #p_switch_cfg_persistence.join(1)
     else:
         logger.info("Persistence-control job is disabled")
+    if cfg.get("job.cegonha_sync_devices", "enabled") == "true":
+        p_cegonha_sync_devices = Process(target=cegonha_sync_devices.daemon,
+                                         name="netl2api [netl2server:http-daemon/job/cegonha-sync-devices]")
+        p_cegonha_sync_devices.start()
+    else:
+        logger.info("Cegonha sync-devices job is disabled")
+    if cfg.get("job.metrics", "enabled") == "true":
+        p_metrics = Process(target=metrics.daemon,
+                                         name="netl2api [netl2server:http-daemon/job/metrics]")
+        p_metrics.start()
+    else:
+        logger.info("Cegonha sync-devices job is disabled")
 
 
 def start():
@@ -463,10 +516,15 @@ def start():
     ps_owner = cfg.get("httpd", "user")
     if ps_owner:
         os.setuid(pwd.getpwnam(ps_owner)[2])
-
+    try:
+        from setproctitle import setproctitle
+    except ImportError:
+        pass
+    else:
+        setproctitle("netl2api [netl2server:http-daemon]")
     logger.info("Starting netl2server...")
     start_workers()
-    run(server=cfg.get("httpd", "handler"), host=cfg.get("httpd", "host"), port=cfg.getint("httpd", "port"))
+    run(server=PasteServerAdapter, host=cfg.get("httpd", "host"), port=cfg.getint("httpd", "port"))
 
 
 def main(action="foreground"):
